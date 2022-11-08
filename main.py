296e7e6e489c4546f997483462fda0b532691add
Marjanjezakon <milanovic.marjan@gmail.com>
2022-11-08 08:44:16 +0100

    Dice rolling simulator that still goes on forever for some reason

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+# Create a condition for the main loop
+game_active = True
+
+
+# Function that rolls the dice and prints out the output
+def response():
+    dice_roll = random.randint(1, 6)
+    print(f"You rolled a {dice_roll}")
+
+
+# Check if the game_active is True, and start the while loop with constant input being asked.
+# Game currently goes on forever for some reason
+while game_active == True:
+    roll = input("Roll the dice? Y / N ")
+    
+    if roll == "Y" or "y":
+        response()
+    elif roll == "N" or "n":
+        quit()
+    else:
+        print("Please enter the proper value.")
